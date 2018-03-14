9c7e71f83e007fba47fa01bd6d71a573a2cc2306
gabrielchase <gchase.patron@gmail.com>
2018-03-14 23:40:25 +0800

    Finish `get_flames_count` and make `HOST` a global variable

--- FlamesServer.py
+++ FlamesServer.py
@@ -1,10 +1,23 @@
 from __future__ import print_function
 import socket
+import collections
 
 QUIT = 'quit'
 QUIT_TRIGGER = 'q'
 RECEIVED = 'received'
+HOST = 55554
 
+
+def get_flames_count(data):
+    split_data = data.split(',')
+    w1 = collections.Counter(split_data[0])
+    w2 = collections.Counter(split_data[1])
+    
+    unique_letters_list = list((w1 - w2).elements()) + list((w2 - w1).elements())
+    unique_letters = ''.join(unique_letters_list).strip()
+
+    print('Unique Letters: ', unique_letters)
+    return len(unique_letters)
 
 def start_server():
     while True:
@@ -15,14 +28,15 @@ def start_server():
             conn.close()
             break
         else:
-            print('DATA: {}'.format(data))
+            flames_count = get_flames_count(data)
+            print('flames_count: {}\n'.format(flames_count))
             conn.send(RECEIVED)
 
 
 
 if __name__ == '__main__':
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('', 58913))
+    server_socket.bind(('', HOST))
     server_socket.listen(5) 
     conn, _ = server_socket.accept()
 
