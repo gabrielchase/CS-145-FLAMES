7b7aade32b2875f6935f1b375079f3fad08c6802
gabrielchase <gchase.patron@gmail.com>
2018-03-15 11:40:38 +0800

    Create caesar cipher decrypt with `EncryptedFlamesServer.py`

---
+++ EncryptedFlamesServer.py
@@ -0,0 +1,78 @@
+from __future__ import print_function
+import socket
+import collections
+
+QUIT = 'quit'
+QUIT_TRIGGER = 'q'
+FLAMES = 'FLAMES'
+FLAMES_DICT = {
+    'F': 'Friendship',
+    'L': 'Love',
+    'A': 'Affection',
+    'M': 'Marriage',
+    'E': 'Enemy',
+    'S': 'Sibling'
+}
+SHIFT_KEY = 7
+HOST = 55555
+
+
+def caesar_cipher_decrypt(cipher_text, shift_key=SHIFT_KEY):
+    plain_text = ''
+    
+    for ch in cipher_text:
+        if ch.isalpha():
+            shifted_char = chr((ord(ch) - 97 - shift_key % 26) % 26 + 97)
+            plain_text += shifted_char
+        else:
+            plain_text += ch
+    
+    return plain_text
+
+def get_flames_count(data):
+    split_data = data.split(',')
+    w1 = collections.Counter(split_data[0])
+    w2 = collections.Counter(split_data[1])
+    unique_letters_list = list((w1 - w2).elements()) + list((w2 - w1).elements())
+    unique_letters = ''.join(unique_letters_list).strip().replace(' ', '')
+
+    print('Unique Letters: {}'.format(unique_letters))
+    return len(unique_letters)
+
+def get_relationship(flames_count, current_idx=0, current_flames=FLAMES):
+    if len(current_flames) == 1:
+        return FLAMES_DICT.get(current_flames)
+    else:
+        current_idx = (current_idx + flames_count - 1) % len(current_flames)
+        current_flames = current_flames[:current_idx] + current_flames[current_idx + 1:]
+
+        return get_relationship(flames_count, current_idx, current_flames)
+
+def start_server():
+    while True:
+        data = conn.recv(64)
+        if data == QUIT_TRIGGER:
+            print('--- Closing connection ---')
+            conn.send(QUIT)
+            conn.close()
+            break
+        else:
+            decrypted_data = caesar_cipher_decrypt(data)
+            flames_count = get_flames_count(decrypted_data)
+            relationship = get_relationship(flames_count=flames_count)
+            
+            print('flames_count: {}'.format(flames_count))
+            print('relationship: {}\n'.format(relationship))
+            
+            conn.send(relationship)
+
+
+if __name__ == '__main__':
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('', HOST))
+    server_socket.listen(5) 
+    conn, _ = server_socket.accept()
+
+    start_server()
+
+    server_socket.close()
